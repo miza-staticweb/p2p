4639a0df6b9efbf01bcaaaa8df636ea1667df552
Minzhang Wei <minzhang@dishbranch-lm.(none)>
2014-12-04 15:51:09 +0800

    code base

--- static/js/user.js
+++ static/js/user.js
@@ -1,3 +1,13 @@
 $(document).ready(function() {
-    
+    $('.user-menu').delegate('.nav-item', 'click', function(event) {
+        var container = $(this).closest('li');
+        container.find('.submenu').toggle(200);
+        event.preventDefault();
+    });
+    $('.user-information').delegate('a.info-ops', 'click', function(event) {
+        var container = $(this).closest('.info-item');
+        container.toggleClass('info-form-active');
+        container.find('.info-form').toggle(200);
+    });
+    $('#div').hih
 });
